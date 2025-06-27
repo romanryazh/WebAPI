049ca3939f36c2ad820086d15178e1704c3e13c1
Roman Ryazh <romanryazh@yandex.ru>
2025-06-27 03:42:19 +0300

    Added simple user authorization

---
+++ WebAPI/Middlewares/ErrorMiddleware.cs
@@ -0,0 +1,34 @@
+﻿namespace WebAPI.Middlewares;
+
+public class ErrorMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        await next(context);
+
+        switch (context.Response.StatusCode)
+        {
+            case 401:
+            {
+                await context.Response.WriteAsync("Unauthorized");
+                break;
+            }
+            case 404:
+            {
+                await context.Response.WriteAsync("Not Found");
+                break;
+            }
+            case 419:
+            {
+                await context.Response.WriteAsync("Authentication Timeout");
+                break;
+            }
+            default:
+            {
+                await context.Response.WriteAsync($"\r \n Token {context.Request.Query["token"]} is valid");
+                break;
+            }
+                
+        }
+    }
+}
---
+++ WebAPI/Middlewares/RouterMiddleware.cs
@@ -0,0 +1,20 @@
+﻿namespace WebAPI.Middlewares;
+
+public class RouterMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var path = context.Request.Path;
+        if (path == "/")
+        {
+            await context.Response.WriteAsync("Hello");
+        }
+        else
+        {
+            if (path == "/admin") 
+                await context.Response.WriteAsync("Admin Panel");
+            else
+                context.Response.StatusCode = 404;
+        }
+    }
+}
